d5d5867ed9e2cd828bd18fb4c9a726d06372d598
Mimori <mimorimori256@gmail.com>
2026-01-08 13:18:06 +0900

    Use Baseline browserslist query instead of manual targets

--- rollup.config.js
+++ rollup.config.js
@@ -28,13 +28,7 @@ const configurePlugins = ({module}) => {
             targets: [
               // Match browsers supporting "Baseline Widely Available" features:
               // https://web.dev/baseline
-              'chrome >0 and last 2.5 years',
-              'edge >0 and last 2.5 years',
-              'safari >0 and last 2.5 years',
-              'firefox >0 and last 2.5 years',
-              'and_chr >0 and last 2.5 years',
-              'and_ff >0 and last 2.5 years',
-              'ios >0 and last 2.5 years',
+              'baseline widely available',
             ],
           },
         ],
